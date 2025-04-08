69b99a42ac02cd94eba5fbe6e55c20fe9eaf028c
Rishit Gondaliya <mrrishitgondaliya@gmail.com>
2025-04-08 10:28:17 +0530

    prevent going back by browser back button

--- public/js/preventBack.js
+++ public/js/preventBack.js
@@ -1,15 +1,15 @@
-// (function () {
-//   if (window.history.replaceState) {
-//     window.history.replaceState(null, null, window.location.href);
-//   }
+// if (window.history && window.history.pushState) {
+//   window.history.pushState(null, "", window.location.href);
 //   window.onpopstate = function () {
-//     window.history.go(1); // Prevents going back
+//     // When back button is pressed
+//     window.history.pushState(null, "", window.location.href); // redirect to your home route
 //   };
-// })();
+// }
 
 (function () {
   history.pushState(null, null, location.href);
   window.onpopstate = function () {
-    history.go(1); // Forces the user to stay on the current page
+    // console.log(history)
+    history.go(0); // Forces the user to stay on the current page
   };
 })();
