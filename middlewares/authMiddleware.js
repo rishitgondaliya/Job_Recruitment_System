acee957de9857744d6877fd773ead244c0f30be7
Rishit Gondaliya <mrrishitgondaliya@gmail.com>
2025-04-08 10:26:43 +0530

    storing user in req object

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,30 +1,28 @@
 const jwt = require("jsonwebtoken");
+const User = require("../models/user");
 
-const authenticateJWT = (req, res, next) => {
+const verifyToken = async (req, res, next) => {
   const token = req.cookies.token;
 
   if (!token) {
-    // console.log(token)
-    return res.status(401).json({ message: "Unauthorized ! Access denied" });
-  }
-
-  try {
-    // console.log(token)
-    const verified = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = verified;
-    next();
-  } catch (err) {
-    console.error("JWT Verification Error:", err.message);
-    return res.status(401).json({ message: "Invalid or expired token" });
-  }
-};
-
-const isAuthenticated = (req, res, next) => {
-  if (!req.session.isLoggedIn) {
     return res.redirect("/auth/login");
   }
-  next();
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await User.findById(decoded.id);
+    if (!user) {
+      return res.redirect("/auth/login");
+    }
+    
+    req.user = user;
+    // console.log("req.user", req.user)
+    next();
+  } catch (err) {
+    console.error("JWT verification failed:", err);
+    res.clearCookie("token");
+    return res.redirect("/auth/login");
+  }
 };
 
-module.exports = authenticateJWT;
-module.exports = isAuthenticated
+module.exports = verifyToken;
