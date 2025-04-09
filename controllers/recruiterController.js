c86cb804017b59181e2943a0d23a16d69a26db2a
Rishit Gondaliya <mrrishitgondaliya@gmail.com>
2025-04-09 10:24:08 +0530

    enabled recruiter to edit job post

--- controllers/recruiterController.js
+++ controllers/recruiterController.js
@@ -62,7 +62,7 @@ exports.postAddNewJob = async (req, res, next) => {
       return res.render("recruiter/addNewJob", {
         pageTitle: "Add new job",
         path: "/addNewJob",
-        errors: { categoryName: "Plese select category." },
+        errors: { categoryName: "Please select category." },
         formData: req.body,
         categories,
       });
@@ -87,6 +87,7 @@ exports.postAddNewJob = async (req, res, next) => {
       recruiterId: req.user._id,
       categoryId: category._id,
       category: categoryName,
+      company: req.user.company,
       jobDetail: jobDetail,
     });
 
@@ -103,8 +104,8 @@ exports.postAddNewJob = async (req, res, next) => {
     if (err.name === "ValidationError") {
       for (let field in err.errors) {
         // Use short field names if needed, or use full path
-        const shortKey = field.split(".").pop();
-        errors[shortKey] = err.errors[field].message;
+        // const shortKey = field.split(".").pop();
+        errors[field] = err.errors[field].message;
       }
     } else {
       console.log("Unexpected error:", err);
@@ -133,15 +134,24 @@ exports.getJobPosts = async (req, res) => {
       });
     }
     jobListings = await jobListing.find({ recruiterId: req.user._id });
+    // res.cookie("successMessage", "Job posts fetched successfully", {
+    //   maxAge: 3000,
+    //   httpOnly: false,
+    // });
     return res.render("recruiter/jobPosts", {
       pageTitle: "Job Posts",
       path: "/jobPosts",
       jobListings,
+      successMessage: "Job posts fetched successfully",
       errors: {},
       formData: {},
     });
   } catch (err) {
     console.error("Error fetching jobPosts:", err);
+    res.cookie("errorMessage", "Error fetching jobPosts", {
+      maxAge: 3000,
+      httpOnly: false,
+    });
     return res.render("recruiter/jobPosts", {
       pageTitle: "Job Posts",
       path: "/jobPosts",
@@ -200,10 +210,11 @@ exports.deleteJobPost = async (req, res, next) => {
 };
 
 exports.getEditJobPost = async (req, res, next) => {
+  let categories = await Category.find().select("name");
   try {
-    const jobPostId = req.params.jobPostId
-    const jobPost = await jobListing.findById(jobPostId)
-    // console.log(jobPost)
+    const jobPostId = req.params.jobPostId;
+    const jobPost = await jobListing.findById(jobPostId);
+    // console.log("getJobPost", jobPost);
     if (!jobPost) {
       return res.status(404).render("recruiter/jobPosts", {
         pageTitle: "Job Posts",
@@ -211,30 +222,113 @@ exports.getEditJobPost = async (req, res, next) => {
         jobListings: await jobListing.find({ recruiterId: req.user._id }),
         errors: {},
         errorMessage: "Job post not found.",
-      })
+      });
     }
-    if (!req.user || String(req.user._id) !== String(jobPost.recruiterId)){
+    if (!req.user || String(req.user._id) !== String(jobPost.recruiterId)) {
       return res.status(403).render("500", {
         pageTitle: "Unauthorized",
         path: "/500",
-        errorMessage: "Access denied! You are not authorized to edit this job post."
-      })
+        errorMessage:
+          "Access denied! You are not authorized to edit this job post.",
+      });
     }
+    // console.log("categories", categories)
     return res.render("recruiter/editJobPost", {
       pageTitle: "Edit Job Post",
-      path: '/jobPosts',
-      jobPost: jobPost.jobDetail,
+      path: "/jobPosts",
+      jobPost: jobPost,
       errors: {},
-      categories: await Category.find().select("name")
-    })
-  } catch(err){
-    console.error("Error getting edit job post:", err)
+      categories,
+    });
+  } catch (err) {
+    console.error("Error getting edit job post:", err);
     return res.status(500).render("recruiter/jobPosts", {
       pageTitle: "Job Posts",
       path: "/jobPosts",
       jobListings: await jobListing.find({ recruiterId: req.user._id }),
       errors: {},
-      errorMessage: "An error occurred while getting the job post."
-    })
+      errorMessage: "An error occurred while getting the job post.",
+    });
   }
-}
+};
+
+exports.postEditJobPost = async (req, res, next) => {
+  const jobPostId = req.body.jobPostId;
+  let jobPost;
+  let categories = await Category.find().select("name");
+  try {
+    console.log("jobPostId:", jobPostId);
+    const updatedJobPost = {
+      // recruiterId: req.user._id,
+      // categoryId: ,
+      category: req.body.categoryName,
+      jobDetail: {
+        jobTitle: req.body.jobTitle,
+        description: req.body.description,
+        requirements: Array.isArray(req.body.requirements)
+          ? req.body.requirements
+          : req.body.requirements.split(",").map((r) => r.trim()),
+        salary: req.body.salary,
+        locationType: req.body.locType,
+        location: req.body.location,
+        vacancy: req.body.vacancy,
+        experience: req.body.experience,
+      },
+    };
+    jobPost = await jobListing.findById(jobPostId);
+    if (!jobPost) {
+      return res.status(404).render("recruiter/jobPosts", {
+        pageTitle: "Job Posts",
+        path: "/jobPosts",
+        jobListings: await jobListing.find({ recruiterId: req.user._id }),
+        errors: {},
+        errorMessage: "Job post not found.",
+      });
+    }
+
+    // console.log("jobPost:", jobPost);
+    // console.log("updatedJobPost:", updatedJobPost);
+    await jobListing.findByIdAndUpdate(
+      jobPostId,
+      { $set: updatedJobPost },
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+
+    res.cookie("editSuccess", "Job post updated successfully.", {
+      maxAge: 3000,
+      httpOnly: false,
+    });
+    return res.redirect("/recruiter/jobPosts");
+  } catch (error) {
+    console.error("Update Error:", error);
+
+    if (error.name === "ValidationError") {
+      const errors = {};
+      for (let field in error.errors) {
+        errors[field] = error.errors[field].message;
+      }
+
+      console.log("errors:", errors);
+
+      return res.status(422).render("recruiter/editJobPost", {
+        pageTitle: "Edit job post",
+        path: "/jobPosts",
+        jobPost: jobPost,
+        categories,
+        errors,
+        errorMessage: "Validation failed. Please correct the fields.",
+      });
+    }
+
+    return res.status(500).render("recruiter/jobPosts", {
+      pageTitle: "Job Posts",
+      path: "/jobPosts",
+      jobListings: await jobListing.find({ recruiterId: req.user._id }),
+      errorMessage: "An error occurred while updating the job post.",
+      errors: {},
+    });
+  }
+};
