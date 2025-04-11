ccc4c7463f68bc96cb7adc3f568e4b8a816e90d9
Rishit Gondaliya <mrrishitgondaliya@gmail.com>
2025-04-11 11:04:31 +0530

    added logic for filter jobs, refactored postSaveJob logic, added next and prev buttons on job detail page

--- controllers/jobSeekerController.js
+++ controllers/jobSeekerController.js
@@ -53,9 +53,20 @@ exports.getJobCategories = async (req, res, next) => {
 
 exports.getAllJobs = async (req, res, next) => {
   try {
+    const { category, locationType, experience, salary } = req.query;
+
+    const filter = {};
+
+    if (category) filter.category = category;
+    if (locationType) filter["jobDetail.locationType"] = locationType;
+    if (experience) filter["jobDetail.experience"] = { $lte: experience };
+    if (salary) filter["jobDetail.salary"] = { $gte: Number(salary) };
+
+    // console.log("filter", filter);
+    const jobPosts = await jobListing.find(filter).sort({ updatedAt: -1 });
     const categories = await Category.find();
-    const jobPosts = await jobListing.find();
-    console.log("jobPosts", jobPosts);
+    // const jobPosts = await jobListing.find();
+    // console.log("jobPosts", jobPosts);
     if (!jobPosts) {
       return res.status(404).render("500", {
         pageTitle: "No Jobs Found",
@@ -68,6 +79,10 @@ exports.getAllJobs = async (req, res, next) => {
       path: "/allJobs",
       jobPosts,
       categories,
+      selectedCategory: category || "",
+      selectedLocationType: locationType || "",
+      selectedExperience: experience || "",
+      selectedSalary: salary || "",
       // successMessage: "All jobs fetched successfully",
     });
   } catch (error) {
@@ -78,49 +93,77 @@ exports.getAllJobs = async (req, res, next) => {
 
 exports.postSaveJobPost = async (req, res, next) => {
   const jobPostId = req.body.jobPostId;
-  const jobPost = await jobListing.findById(jobPostId);
-  const jobPosts = await jobListing.find();
-  if (!jobPost) {
-    return res.status(404).render("jobSeeker/allJobs", {
-      pageTitle: "All jobs",
-      path: "/allJobs",
-      errorMessage: "Job post not found.",
+
+  try {
+    const jobPost = await jobListing.findById(jobPostId);
+    const categories = await Category.find().select("name");
+
+    if (!jobPost) {
+      return res.status(404).render("jobSeeker/allJobs", {
+        pageTitle: "All jobs",
+        path: "/allJobs",
+        categories,
+        selectedCategory: "",
+        selectedLocationType: "",
+        selectedExperience: "",
+        selectedSalary: "",
+        errorMessage: "Job post not found.",
+      });
+    }
+
+    const alreadySaved = await savedJobs.findOne({
+      "user.userId": req.user._id,
+      "jobDetail.jobId": jobPostId,
     });
+
+    if (alreadySaved) {
+      res.cookie("errorMessage", "You have already saved this job post.", {
+        maxAge: 3000,
+        httpOnly: false,
+      });
+      return res.redirect("/jobSeeker/allJobs"); //
+    }
+
+    const user = {
+      userId: req.user._id,
+      name: req.user.firstName + " " + req.user.lastName,
+    };
+
+    const jobDetail = {
+      jobId: jobPost._id,
+      jobTitle: jobPost.jobDetail.jobTitle,
+      company: jobPost.company,
+    };
+
+    const newSavedJob = new savedJobs({
+      user,
+      jobDetail,
+    });
+
+    await newSavedJob.save();
+
+    res.cookie("successMessage", "Job saved successfully", {
+      maxAge: 3000,
+      httpOnly: false,
+    });
+
+    return res.redirect("/jobSeeker/allJobs");
+  } catch (error) {
+    console.error("Error saving job:", error);
+    next({ message: "Something went wrong. Please try again later." });
   }
-
-  let user = {
-    userId: req.user._id,
-    name: req.user.firstName + " " + req.user.lastName,
-  };
-  let jobDetail = {
-    jobId: jobPost._id,
-    jobTitle: jobPost.jobDetail.jobTitle,
-    company: jobPost.company,
-  };
-
-  const newSavedJob = new savedJobs({
-    user,
-    jobDetail,
-  });
-
-  await newSavedJob.save();
-  res.cookie("successMessage", "Job saved successfully", {
-    maxAge: 3000,
-    httpOnly: false,
-  });
-  res.redirect("/jobSeeker/allJobs");
-  // res.render('jobSeeker/allJobs', {
-  //   pageTitle: "All Jobs",
-  //   path: '/allJObs',
-  //   jobPosts,
-  //   errors: {},
-  //   successMessage: "Job saved successfully."
-  // })
 };
 
 exports.getJobDetails = async (req, res, next) => {
   const jobPostId = req.params.jobPostId;
   const jobPost = await jobListing.findById(jobPostId);
+
+  const allJobs = await jobListing.find({}, "_id").sort({ updatedAt: -1 });
+  const jobIndex = allJobs.findIndex((j) => j._id.toString() === jobPostId);
+  const prevJobId = jobIndex > 0 ? allJobs[jobIndex - 1]._id : null;
+  const nextJobId =
+    jobIndex < allJobs.length - 1 ? allJobs[jobIndex + 1]._id : null;
+
   if (!jobPost) {
     res.render("/jobSeeker/allJobs", {
       pageTitle: "All jobs",
@@ -132,8 +175,10 @@ exports.getJobDetails = async (req, res, next) => {
   // console.log("jobPost",jobPost)
   res.render("jobSeeker/jobDetail", {
     pageTitle: jobPost.jobDetail.jobTitle,
-    jobPost,
     path: "/allJobs",
+    jobPost,
+    prevJobId,
+    nextJobId,
     errors: {},
   });
 };
