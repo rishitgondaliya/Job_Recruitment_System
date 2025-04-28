16e8bf78f66fb3043693946a0558f32890f0e806
Rishit Gondaliya <mrrishitgondaliya@gmail.com>
2025-04-28 10:29:14 +0530

    modified pre for error handling

--- models/user.js
+++ models/user.js
@@ -1,5 +1,14 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 const { Schema } = mongoose;
+
+// Utility to format names to Title Case
+function toTitleCase(str) {
+  return str
+    .split(" ")
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+    .join(" ");
+}
 
 const userSchema = new Schema(
   {
@@ -15,8 +24,8 @@ const userSchema = new Schema(
       type: String,
       trim: true,
       required: [true, "First Name is required"],
-      minlength: [2, "firstname must be at least 2 characters long"],
-      maxlength: [15, "firstname must not be longer than 15 characters"],
+      minlength: [2, "First name must be at least 2 characters long"],
+      maxlength: [15, "First name must not be longer than 15 characters"],
       match: [
         /^[A-Za-z\s'-]+$/,
         "Name can only contain alphabets, spaces, hyphens, and apostrophes",
@@ -26,8 +35,8 @@ const userSchema = new Schema(
       type: String,
       trim: true,
       required: [true, "Last Name is required"],
-      minlength: [2, "lastname must be at least 2 characters long"],
-      maxlength: [15, "lastname must not be longer than 15 characters"],
+      minlength: [2, "Last name must be at least 2 characters long"],
+      maxlength: [15, "Last name must not be longer than 15 characters"],
       match: [
         /^[A-Za-z\s'-]+$/,
         "Name can only contain alphabets, spaces, hyphens, and apostrophes",
@@ -43,37 +52,28 @@ const userSchema = new Schema(
       type: String,
       trim: true,
       lowercase: true,
+      unique: [true, "User with the same email already exists"],
+      required: [true, "Email is required"],
       validate: {
         validator: function (v) {
           return /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(v);
         },
         message: "Please enter a valid email",
       },
-      unique: [true, "User with same email already exists"],
-      required: [true, "Email is required"],
     },
     phone: {
       type: String,
+      required: [true, "User phone number is required"],
       validate: {
         validator: function (v) {
           return /^\+?[1-9]\d{9}$/.test(v);
         },
-        message: (props) => `${props.value} is not a valid phone number!`,
+        message: props => `${props.value} is not a valid phone number!`,
       },
-      required: [true, "User phone number is required"],
     },
     password: {
       type: String,
-      minlength: [8, "Password must have at least 8 characters long"],
       required: [true, "User password is required"],
-      validate: [
-        {
-          validator: function (v) {
-            return v !== this.email;
-          },
-          message: "Email & Password can not be the same",
-        },
-      ],
     },
     isActive: {
       type: Boolean,
@@ -93,27 +93,26 @@ const userSchema = new Schema(
   { timestamps: true }
 );
 
-userSchema.pre("save", function (next) {
-  if (this.firstName) {
-    this.firstName = this.firstName
-      .split(" ")
-      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-      .join(" ");
+// Pre-save hook to format names, validate & hash password
+userSchema.pre("save", async function (next) {
+  if (this.firstName) this.firstName = toTitleCase(this.firstName);
+  if (this.lastName) this.lastName = toTitleCase(this.lastName);
+  if (this.company) this.company = toTitleCase(this.company);
+
+  if (!this.isModified("password")) return next();
+
+  const passwordValidationPattern =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+  if (!passwordValidationPattern.test(this.password)) {
+    return next(
+      new Error(
+        "Password must have at least 8 characters, including an uppercase letter, a lowercase letter, a number, and a special character."
+      )
+    );
   }
 
-  if (this.lastName) {
-    this.lastName = this.lastName
-      .split(" ")
-      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-      .join(" ");
-  }
-
-  if (this.company) {
-    this.company = this.company
-      .split(" ")
-      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-      .join(" ");
-  }
+  this.password = await bcrypt.hash(this.password, 12);
   next();
 });
 
